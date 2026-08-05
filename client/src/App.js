34c89733e64d2bc19ac24fc1b013e043f953dbe6
einn-dev <Norhidayala@gmail.com>
2026-08-05 11:23:11 +0800

    delete demo script for MongoDB installation and usage

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
       <Router>
         <Routes>
           {/* Public Routes */}
-          <Route path="/"           element={<Navigate to="/login" replace />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/login"      element={<LoginPage />} />
           <Route path="/register"   element={<RegisterPage />} />
           <Route path="/unauthorized" element={<Unauthorized />} />
